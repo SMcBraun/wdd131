1c3ec307ce82841fce4c6c0d9bd6d25da41c76c6
SMcBraun <smcbraun@byupathway.edu>
2026-05-21 11:34:06 -1000

    git add . git commit -m "Removed duplicate WDD131 folder" git push

--- WDD131/scripts/getdates.js
+++
@@ -1,8 +0,0 @@
-const year = new Date().getFullYear();
-
-document.getElementById("currentyear").textContent = year;
-
-document.getElementById("lastModified").textContent =
-    "Last Modified: " + document.lastModified;
-
-
