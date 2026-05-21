7bb6e11d52b746e5b92717f095117c7f16d93d39
SMcBraun <smcbraun@byupathway.edu>
2026-05-21 09:51:04 -1000

    folder revision for correct file wdd131 use only

---
+++ WDD131/scripts/getdates.js
@@ -0,0 +1,8 @@
+const year = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = year;
+
+document.getElementById("lastModified").textContent =
+    "Last Modified: " + document.lastModified;
+
+
---
+++ WDD131/week02/scripts/temples.js
@@ -0,0 +1,12 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+document.querySelector('#year').textContent = new Date().getFullYear();
+
+document.querySelector('#lastModified').textContent =
+    `Last Modified: ${document.lastModified}`;
