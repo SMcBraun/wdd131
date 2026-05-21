0250de2c57e085c1fd17113966492d7e122e211c
SMcBraun <smcbraun@byupathway.edu>
2026-05-21 09:42:48 -1000

    Add week03 homework files

---
+++ week03/place.js
@@ -0,0 +1,35 @@
+const temperature = 28;
+const windspeed = 10;
+
+function calculateWindChill(temp, speed) {
+    return (
+        13.12 +
+        0.6215 * temp -
+        11.37 * Math.pow(speed, 0.16) +
+        0.3965 * temp * Math.pow(speed, 0.16)
+    ).toFixed(1);
+}
+const windChill = document.querySelector("#windchill");
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill.textContent = ${ calculatorWindChill(temperature, windSpeed) }°C;
+} else {
+    windChill.textContent = "N/A";
+
+}
+
+// Footer year
+document.getElementById("year").textContent = new Date().getFullYear();
+
+//Last modified date
+document.getElementById("lastModified").textContent =
+    Last Modified: $(document.lastModified} ;
+
+
+
+document.querySelector("#year").textContent =
+    new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+    Last Modified: ${ document.lastModified };
+
