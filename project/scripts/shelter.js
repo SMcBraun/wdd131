a7f8fd3f694d2e9478f364a452925d1da38016f4
SMcBraun <smcbraun@byupathway.edu>
2026-06-12 18:46:07 -1000

    Add required JavaScript features

---
+++ project/scripts/shelter.js
@@ -0,0 +1,75 @@
+const dashboardStages = {
+    { label: "Total Animals", value: "1,2248" },
+{ label: "Avaialable for Adoption", value: "342" },
+{ label: "Adopted This Month", value: "156" },
+}
+
+const spotlightAnimal = {
+    name: "Kona",
+    island: "Oahu",
+    age: 2,
+    waitDays: 147
+};
+
+function displayDashboard() {
+    const dashboard = document.querySelector(#dashboard - cards");
+
+    if (dashboard) {
+        let cards = "";
+
+        dashboardStats.forEach(function (stat) {
+            cards += <p><strong>${stat.label}:</strong> ${stat.value}</p>
+        });
+
+        dashboard.innerHTML = cards;
+    }
+}
+
+function displayAnimalSpotlight() {
+    const spotlight = document.querySelector("#animal-spotlight");
+
+    if (spotlight) {
+        let status = "";
+
+        if (spotlightAnimal.waitDays > 100) {
+            status = "Long-Term Resident";
+
+        } else {
+            status = "Recenly Added";
+        }
+
+        spotlight.innerHTML = 
+        <h3>${spotlightAnimal.name}</h3>
+        <p><strong>Island:</strong> ${spotlightAnimal.island}</p>
+        <p><strong>Age:</strong> ${spotlightAnimal.age} years old</p>
+        <p><strong>Waiting:</strong> ${spotlightAnimal.waitDays} days</p>
+        <p><strong>Status:</strong> ${status}</p>
+        <button id="save-favorite">Save Favorite Animal</button>
+        <p id="favorite-message"></p>
+
+            ;
+
+        const button = document.querySelector(#save - favorite");
+    const message = document.querySelector("#favorite-message");
+
+        button.addEventListener("click", function () {
+            localStorage.setItem("favoriteAnimal", spotlightAnimal.name);
+            message.textContent = ${ spotlightAniimal.name } has been saved as your favorite AnimationPlaybackEvent. ;
+        });
+
+    }
+
+}
+
+displayDashboard();
+displayAnimalSpotlight();
+
+
+
+
+
+
+
+
+
+
